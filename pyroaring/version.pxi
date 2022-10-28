8df56cb5f72970ecf058dffcc53448c21b34b2d4
Sam Hames <s.hames@uq.edu.au>
2022-10-28 15:35:12 +1000

    Simplify setup.py by only building cython output files
    - This simplified building by only having one case to consider
      and avoids the need for Cython in the output
    - Explicitly tracks the pyroaring.cpp output from Cython as part
      of the source

---
+++ pyroaring/version.pxi
@@ -0,0 +1,2 @@
+__version__ = "0.3.4dev1"
+__croaring_version__ = "v0.7.1"
--- setup.py
+++ setup.py
@@ -8,41 +8,11 @@ import sys
 import subprocess
 import platform
 
-VERSION = '0.3.3'
+VERSION = '0.3.4dev1'
 PKG_DIR = 'pyroaring'
 
 PLATFORM_WINDOWS = (platform.system() == 'Windows')
 PLATFORM_MACOSX = (platform.system() == 'Darwin')
-
-
-def chdir(func, directory):
-    old_dir = os.getcwd()
-    os.chdir(directory)
-    res = func()
-    os.chdir(old_dir)
-    return res
-
-
-def run(args):
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE)
-    stdout, stderr = proc.communicate()
-    if proc.returncode != 0:
-        sys.exit('Error with the command %s.\n' % ' '.join(args))
-    return stdout.decode('ascii').strip()
-
-
-def git_version():
-    return run(['git', 'rev-parse', 'HEAD'])
-
-
-def git_tag():
-    return run(['git', 'describe', '--tags', '--always', '--dirty'])
-
-
-def write_version(filename, version_dict):
-    with open(filename, 'w') as f:
-        for version_name in version_dict:
-            f.write('%s = "%s"\n' % (version_name, version_dict[version_name]))
 
 
 # Remove -Wstrict-prototypes option
@@ -60,22 +30,10 @@ except (IOError, ImportError, RuntimeError):
     print('Could not generate long description.')
     long_description = ''
 
-USE_CYTHON = os.path.exists(os.path.join(PKG_DIR, 'pyroaring.pyx'))
-if USE_CYTHON:
-    print('Building pyroaring from Cython sources.')
-    from amalgamation import amalgamate
-    amalgamate(PKG_DIR)
-    from Cython.Build import cythonize
-    ext = 'pyx'
-    write_version(os.path.join(PKG_DIR, 'version.pxi'), {
-        '__version__': VERSION,
-        '__git_version__': git_version(),
-        '__croaring_version__': chdir(git_tag, 'CRoaring'),
-        '__croaring_git_version__': chdir(git_version, 'CRoaring'),
-    })
-else:
-    print('Building pyroaring from C sources.')
-    ext = 'cpp'
+
+print('Building pyroaring from C sources.')
+ext = 'cpp'
+
 
 if PLATFORM_WINDOWS:
     compile_args = []
@@ -105,14 +63,10 @@ pyroaring = Extension('pyroaring',
                       sources=[filename, os.path.join(PKG_DIR, 'roaring.c')],
                       extra_compile_args=compile_args,
                       )
-if USE_CYTHON:
-    pyroaring = cythonize(pyroaring, compiler_directives={'binding': True})
-else:
-    pyroaring = [pyroaring]
 
 setup(
     name='pyroaring',
-    ext_modules=pyroaring,
+    ext_modules=[pyroaring],
     version=VERSION,
     description='Fast and lightweight set for unsigned 32 bits integers.',
     long_description=long_description,
