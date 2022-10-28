243dc854041868535aa1cf18e91049024231b2b8
Sam Hames <s.hames@uq.edu.au>
2022-10-28 14:37:54 +1000

    Remove custom amalgamation script, add script to download official CRoaring release amalgamation

---
+++ download_amalgamation.py
@@ -0,0 +1,13 @@
+"""
+Helper script to download the latest amalgamation files for CRoaring.
+
+"""
+import os
+from urllib.request import urlretrieve
+
+latest_release = "https://github.com/RoaringBitmap/CRoaring/releases/latest/download/"
+
+files = ["roaring.c", "roaring.h"]
+
+for file in files:
+	r = urlretrieve(latest_release + file, os.path.join("pyroaring", file))
