08dedb602d2850c0413278ec346cd9508e2898b3
Sam Hames <s.hames@uq.edu.au>
2022-11-04 11:09:31 +1000

    Test Windows amalgamation build against an earlier amalgamation version

--- download_amalgamation.py
+++ download_amalgamation.py
@@ -1,15 +1,24 @@
 """
-Helper script to download the latest amalgamation files for CRoaring.
+Helper script to download a specific release amalgamation file for CRoaring.
+
+Usage: python download_amalgamation.py <croaring_release_version, like v0.6.0>
+
+The version needs to be the specific release tag on github.
 
 """
 import os
 from urllib.request import urlretrieve
+import sys
 
-latest_release = "https://github.com/RoaringBitmap/CRoaring/releases/latest/download/"
+version = sys.argv[1]
+
+release = f"https://github.com/RoaringBitmap/CRoaring/releases/download/{version}/"
+
+print(f"Downloading version {version} of the croaring amalgamation")
 
 files = ["roaring.c", "roaring.h"]
 
 for file in files:
-	r = urlretrieve(latest_release + file, os.path.join("pyroaring", file))
+	r = urlretrieve(release + file, os.path.join("pyroaring", file))
 
 print("If this is a new version of CRoaring, don't forget to update pyroaring/version.pxi")
